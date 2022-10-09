64ad058ea1054d2538169428a638c976d6256789
Daan Koning <daanolivierkoning@gmail.com>
2022-10-09 17:06:28 +0200

    Made sure the head is always being rendered.

--- src/brainfuckpy/visualize.py
+++ src/brainfuckpy/visualize.py
@@ -7,7 +7,7 @@ console = Console()
 CODE_ACCENT = "bold red"
 TAPE_ACCENT = "bold red"
 
-SECOND_PER_REFRESH = 0.01
+SECOND_PER_REFRESH = 0.1
 
 outputs = []
 
@@ -16,12 +16,24 @@ def _visualization_output_callback(byte: int):
 	outputs.append(chr(byte))
 
 
+def get_subset(tape: list[str], centered_at: int, length: int=40) -> list[str]:
+	"""Gets a subset of the tape of length `length` in order to make the visualizer wrap lines better."""
+	minimum = centered_at - length//2
+	maximum = centered_at + length//2
+
+	if minimum < 0 or maximum > brainfuckpy.TAPE_SIZE:
+		return tape[minimum:] + tape[:maximum]
+	else:
+		return tape[minimum:maximum]
+
+
 def write_visualization(program: str, tape: list[int], code_pointer: int, head_position: int):
 	"""Writes a single cycle of the visualization, means clearing and displaying the information."""
-	# constuct the tape
+	# construct the tape
 	tape_with_highlight = [i for i in tape]
 	tape_with_highlight[head_position] = f"[{TAPE_ACCENT}]{tape[head_position]}[/{TAPE_ACCENT}]"
-	tape_text = ", ".join(str(i) for i in tape_with_highlight[:40])  # FIXME: BAD bodge
+	tape_with_highlight = get_subset(tape_with_highlight, head_position)
+	tape_text = ", ".join(str(i) for i in tape_with_highlight)
 
 	# construct the program
 	program_text = program[:code_pointer] + f"[{CODE_ACCENT}]" + program[code_pointer] + f"[/{CODE_ACCENT}]" + program[code_pointer+1:]
