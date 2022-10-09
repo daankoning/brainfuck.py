2dc476356c2883c7b621d82cb3221f32ecc74a9d
Daan Koning <daanolivierkoning@gmail.com>
2022-10-09 16:54:59 +0200

    Changed constuction order, should make the visualizer flash a bit less.

--- src/brainfuckpy/visualize.py
+++ src/brainfuckpy/visualize.py
@@ -18,23 +18,23 @@ def _visualization_output_callback(byte: int):
 
 def write_visualization(program: str, tape: list[int], code_pointer: int, head_position: int):
 	"""Writes a single cycle of the visualization, means clearing and displaying the information."""
-	console.clear()
-
-	# print the tape
+	# constuct the tape
 	tape_with_highlight = [i for i in tape]
 	tape_with_highlight[head_position] = f"[{TAPE_ACCENT}]{tape[head_position]}[/{TAPE_ACCENT}]"
-	console.print(", ".join(str(i) for i in tape_with_highlight[:40]))  # FIXME: BAD bodge
+	tape_text = ", ".join(str(i) for i in tape_with_highlight[:40])  # FIXME: BAD bodge
 
+	# construct the program
+	program_text = program[:code_pointer] + f"[{CODE_ACCENT}]" + program[code_pointer] + f"[/{CODE_ACCENT}]" + program[code_pointer+1:]
+
+	# construct the outputs
+	output_text = "".join(outputs)
+
+	console.clear()
+	console.print(tape_text)
 	console.print("\n")
-
-	# print the program
-	program_str = program[:code_pointer] + f"[{CODE_ACCENT}]" + program[code_pointer] + f"[/{CODE_ACCENT}]" + program[code_pointer+1:]
-	console.print(program_str)
-
+	console.print(program_text)
 	console.print("\n")
-
-	# print the outputs
-	console.print("".join(outputs))
+	console.print(output_text)
 
 	time.sleep(SECOND_PER_REFRESH)
 
