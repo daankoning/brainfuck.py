1fd2fd4b773fa56abbbe5321281c9f08b555fcb8
Daan Koning <daanolivierkoning@gmail.com>
2022-10-15 21:27:44 +0200

    Implemented customisable visualization speed.

--- src/brainfuckpy/__main__.py
+++ src/brainfuckpy/__main__.py
@@ -1,10 +1,11 @@
 from .brainfuck import evaluate_brainfuck
-from .visualize import visualize_evaluation
+from .visualize import visualize_evaluation, set_visualization_refresh_rate
 import sys, os, select
 
 
 def incorrect_usage():
 	sys.stdout.write("Incorrect usage: use the -h flag to display a help message. \n")
+	sys.exit(1)
 
 
 def help_message():
@@ -12,21 +13,36 @@ def help_message():
 	evaluate_brainfuck.py -- A lightweight pure python evaluate_brainfuck interpreter
 
 SYNOPSIS
-	brainfuckpy [-h] [-vis] [file|program]
+	brainfuckpy [-h] [-vis [-vrr rate]] [file|program]
 
 DESCRIPTION
 	Either pipe in, provide as an argument or provide a file containing a valid program.
 	
 	-vis --visualize
-			Run the code visualizer.
+			Run the code visualizer. Optionally the -vrr flag can be used to set a refresh rate of the visualization.
+	-vrr --visualization-refresh-rate
+			Set the amount of times per second the visualizer updates. Provide this refresh rate as the immediate next
+			argument.
 	-h, --help
 			Display this message.\n""")
 
 
 def main():
+	# Please ignore this entire first block, it is distinctly awful
 	evaluator = evaluate_brainfuck
 	if "-vis" in sys.argv or "--visualize" in sys.argv:
 		evaluator = visualize_evaluation
+		try:
+			if "-vrr" in sys.argv:
+				speed_index = sys.argv.index("-vrr") + 1
+				set_visualization_refresh_rate(float(sys.argv[speed_index]))
+			elif "--visualization-refresh-rate" in sys.argv:
+				speed_index = sys.argv.index("--visualization-refresh-rate") + 1
+				set_visualization_refresh_rate(float(sys.argv[speed_index]))
+		except ValueError:
+			incorrect_usage()
+	elif "-vrr" in sys.argv or "--visualization-refresh-rate" in sys.argv:
+		incorrect_usage()
 
 	if len(sys.argv) == 1 and select.select([sys.stdin, ], [], [], 0.0)[0]:
 		prgm = sys.stdin.read()
